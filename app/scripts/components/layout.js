9b65b92d308f1086f89dfdf0397b6c1c86058072
Dhruv Bhatia <dhruv@dhruvbhatia.com>
2014-04-24 23:23:59 +1000

    Init

---
+++ app/scripts/components/layout.js
@@ -0,0 +1,15 @@
+/** @jsx React.DOM */
+
+var React = require('React');
+
+var Layout = React.createClass({
+
+  render: function() {
+    return (
+      <p>Test</p>
+    );
+  }
+
+});
+
+module.exports = Layout;
