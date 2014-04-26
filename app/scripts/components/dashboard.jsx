253b188400cdd6465f65bab805b4d9b45b59a15d
Dhruv Bhatia <dhruv@dhruvbhatia.com>
2014-04-27 01:07:28 +1000

    Server authentication now fully works and checks a user's cookie against the server to determine state > Improved Login view by hiding it when checking login status with the server (it now doesn't have a FOUC type flash on initial load)

---
+++ app/scripts/components/dashboard.jsx
@@ -0,0 +1,44 @@
+/** @jsx React.DOM */
+
+var Dashboard = React.createClass({
+
+  render: function() {
+    return (
+      <p>{this.props.navPos}
+
+
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nulla nisl, fermentum sit amet justo vel, commodo hendrerit metus. Quisque dignissim eu sapien vel ornare. Nulla vulputate dui et lorem vulputate fermentum. Mauris vulputate facilisis urna vel fringilla. Suspendisse congue gravida risus eget aliquet. Cras a imperdiet nulla, sit amet convallis odio. Aenean erat erat, elementum sit amet arcu quis, accumsan commodo sapien. Maecenas lacinia est quis massa ultricies ultricies. Donec mauris mi, accumsan ac erat et, sollicitudin fringilla mauris. Nunc varius fermentum consectetur. Suspendisse mattis dui nec egestas ultrices. Sed sodales id turpis ut sagittis. Suspendisse interdum tristique elit. Phasellus sit amet mauris diam.
+
+Cras vel pellentesque nisi, non aliquam enim. Praesent rhoncus quis lacus eu viverra. Vestibulum sollicitudin sit amet nunc a vulputate. In et dignissim risus. Integer suscipit augue sit amet est sollicitudin, eget placerat lectus fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque lobortis, mi et adipiscing pharetra, eros sem scelerisque tellus, eu viverra sem justo vestibulum tellus. Proin pharetra metus id augue imperdiet, eget ultrices arcu commodo. Etiam dui ligula, accumsan auctor aliquam nec, tristique at est. Sed eget venenatis tortor, quis pulvinar sem. Phasellus in ligula massa. Nullam a eros et lacus dapibus posuere.
+
+Morbi id egestas dolor. Donec non posuere massa. In varius hendrerit augue, quis feugiat diam pulvinar eget. Nunc non gravida lacus. Duis iaculis justo ac enim volutpat commodo. Morbi sagittis orci in nibh blandit, ac pulvinar nibh convallis. Proin ac nisl dui. Nam condimentum cursus odio sit amet tempus. Aenean vestibulum elit a lorem dignissim, in adipiscing arcu rutrum. Vestibulum non varius nisl, a porta elit.
+
+Maecenas sit amet eros eget dui blandit consequat. Donec et sollicitudin risus. Vivamus accumsan ante vel mattis vehicula. Vestibulum eget dolor a velit ultricies ultricies. Curabitur turpis sapien, tempus eu consectetur a, tempus sed lacus. Proin sodales iaculis lorem, bibendum lobortis mauris imperdiet vel. Maecenas a bibendum erat. Morbi pellentesque purus sit amet enim pulvinar ullamcorper. Nunc vitae cursus justo. Suspendisse egestas nibh eget neque vulputate molestie. Aenean sit amet velit a felis pellentesque commodo eu ut nibh. Pellentesque at vehicula metus. Morbi aliquet justo non tellus pretium bibendum. Maecenas porttitor et tortor id interdum. Cras fermentum tellus nec porttitor feugiat. Vestibulum facilisis nec dolor vel volutpat.
+
+Aenean commodo nunc eros, a pharetra sem auctor nec. Quisque at ullamcorper lectus, non sagittis est. Morbi sagittis bibendum enim non gravida. Sed ac sem viverra, commodo dolor ut, tempus lectus. Nunc sit amet aliquam nibh. Praesent ultrices tortor at velit interdum viverra. Duis suscipit erat ac turpis pharetra bibendum. Vivamus molestie nec lectus scelerisque convallis. Curabitur metus purus, faucibus tempor neque ac, convallis lacinia tortor. Morbi in neque pretium, placerat orci interdum, ornare sapien. Quisque ornare diam ac eros molestie, ut pellentesque enim eleifend.
+
+Integer dapibus, sapien in suscipit tristique, risus eros tempor ligula, a malesuada tellus elit eget felis. Morbi viverra elit lectus. Nulla eleifend leo ante, sit amet semper nibh auctor non. Vestibulum laoreet ante vitae justo fringilla, facilisis pulvinar nunc dignissim. Donec venenatis nisl a neque pulvinar viverra. Phasellus ac ante et nisi pulvinar fringilla. Nunc dictum euismod ipsum sit amet elementum.
+
+Aenean eleifend non nisi non venenatis. Sed ac turpis at nisi sodales hendrerit vitae quis velit. Mauris eget tincidunt eros, nec molestie nulla. Proin faucibus dictum vestibulum. Sed a neque quam. Pellentesque aliquet lobortis justo, vitae aliquam velit congue a. Suspendisse volutpat gravida arcu vel euismod.
+
+Vivamus in urna et lectus posuere suscipit. Curabitur ultricies erat diam, vel tristique neque tempus luctus. Sed tempus iaculis mi in sollicitudin. Nullam vestibulum quam leo, sed imperdiet nisi congue at. Mauris volutpat arcu sed urna hendrerit, sit amet commodo arcu scelerisque. Integer molestie cursus mi id ornare. Phasellus ut malesuada arcu, sit amet dapibus odio. Phasellus in convallis justo. Etiam hendrerit commodo nulla ac vehicula. Duis lorem est, sodales at venenatis nec, commodo ac neque. Aliquam tincidunt facilisis nunc. Nullam rutrum bibendum orci, sit amet semper tortor consectetur at.
+
+Donec in urna laoreet, ullamcorper libero eget, convallis dolor. Nullam sodales commodo lectus nec dictum. Suspendisse et facilisis neque, quis ullamcorper libero. Nullam in sapien mattis, volutpat arcu eu, viverra augue. Nullam dictum lorem ut orci eleifend, semper gravida nisl congue. Aenean tempor rutrum justo vitae lacinia. Ut suscipit ipsum a nisi ultricies mollis. Maecenas ac auctor justo. Fusce sit amet ipsum volutpat, hendrerit dolor porttitor, rhoncus neque. Donec quis condimentum enim. Praesent vitae lectus nec dolor vestibulum mattis.
+
+Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam pharetra viverra eros quis dignissim. Fusce vel dui mauris. Pellentesque mollis ante ac fermentum pellentesque. Proin ultrices, nulla ut venenatis dapibus, mi velit commodo purus, in imperdiet odio mauris nec felis. Praesent scelerisque eros id sollicitudin laoreet. Praesent ultricies, purus sit amet bibendum imperdiet, ligula arcu imperdiet justo, a ullamcorper turpis lectus sed dolor. Maecenas nec tempus mauris, quis accumsan nibh.
+
+Vestibulum eleifend blandit lobortis. Sed interdum semper sapien, et lacinia dui suscipit sit amet. Integer elementum felis ut diam tempus, ac facilisis eros posuere. Nullam placerat tristique metus, eget venenatis augue molestie eget. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut egestas mauris nec lacus vehicula, pulvinar fringilla lectus tempor. Sed ut ante arcu.
+
+Ut convallis commodo ipsum quis tempus. Sed pharetra velit eget sollicitudin pretium. Fusce molestie at purus a scelerisque. Nulla facilisi. Morbi felis neque, gravida sed congue sed, dapibus vel augue. Vivamus viverra condimentum luctus. Ut at tellus nec augue convallis congue. Maecenas placerat ultrices lorem, non convallis nibh malesuada vel. Praesent non hendrerit velit, sit amet rutrum massa. Curabitur quis elementum est. Aliquam erat volutpat. Duis pulvinar tellus a consequat auctor. Sed iaculis sed ligula quis hendrerit. Fusce consequat facilisis eros, in tempus tellus vehicula eu.
+
+Duis condimentum at mauris quis interdum. Phasellus imperdiet sodales massa, quis lobortis sem blandit nec. Aliquam nec sapien mauris. Aliquam gravida cursus dolor vitae interdum. Cras at ultrices mi, ut lacinia purus. Morbi gravida sapien in lectus rutrum vehicula. Sed volutpat condimentum ligula. Aliquam erat volutpat. Phasellus aliquet velit felis, non faucibus nunc hendrerit non. Fusce semper urna id nibh mollis, sit amet viverra nibh dictum. Sed dignissim vel lorem vel consequat. Suspendisse convallis nibh tortor, a suscipit metus tincidunt a.
+
+Vivamus interdum sed diam vitae euismod. Sed dignissim a mauris non cursus. Quisque vestibulum, nunc eu pretium fringilla, lacus ante feugiat nibh, eu dictum sapien velit in dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis sem massa. Suspendisse vel bibendum massa, a mattis dui. Praesent massa eros, pretium quis vehicula quis, blandit sit amet libero. Etiam sed dolor mi. Pellentesque fringilla tempus nulla, at hendrerit magna viverra ut. Praesent dapibus tempor orci, ut vehicula justo luctus sit amet. Fusce mattis felis turpis, at rutrum eros cursus et.
+
+Suspendisse sed semper lacus. Aenean lobortis pellentesque mattis. Fusce luctus sagittis leo, ac commodo magna pharetra ut. In hac habitasse platea dictumst. Donec in ornare sem, rutrum ullamcorper magna. Nulla nulla nibh, ullamcorper in lacus a, sodales gravida libero. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum accumsan iaculis eros in aliquam. Praesent ligula risus, facilisis sed pharetra iaculis, lobortis at mi. Curabitur ornare egestas est, eu ultricies risus. Cras ornare volutpat risus euismod tristique. Mauris condimentum eget nulla vitae volutpat. Curabitur vel purus ac erat commodo viverra.
+
+      </p>
+    );
+  }
+
+});
---
+++ app/scripts/components/layout.jsx
@@ -0,0 +1,398 @@
+/** @jsx React.DOM */
+
+var navigation_data = [
+  {name: "Dashboard", url: ""},
+  {name: "Campaigns", url: "campaigns"},
+  {name: "My Account", url: "account"}
+];
+
+
+var Layout = React.createClass({
+  getInitialState: function() {
+
+    // set the pushState to blank if the user arrives to the dashboard, either by going to the root or entering an invalid route
+    if(this.props.navPos === "Dashboard") {
+      router.navigate("");
+    };
+
+    return {navPos: this.props.navPos, loggedIn: undefined, render: false};
+  },
+
+  componentDidMount: function() {
+
+    // figure out if a cookie exists and is a valid session
+    self = this;
+    var cookie = $.cookie("application");
+
+    if(!_.isUndefined(cookie)) {
+
+      var token = JSON.parse(cookie).sessionId;
+      var email = JSON.parse(cookie).email;
+
+      superagent
+      .get('http://localhost:9000/api/v1/auth/validate/' + token)
+      .set('X-API-Key', token)
+      .set('Accept', 'application/json')
+      .end(function(error, res){
+
+        if(res.ok === true) {
+
+          self.setState({loggedIn: email, render: true});
+
+        } else {
+
+          // Cookie no longer valid - render login form
+          self.setLoggedIn(undefined);
+          self.setState({render: true});
+
+        }
+
+        console.log(res);
+
+      });
+
+    } else {
+
+      // No prev cookie - render login form
+      self.setLoggedIn(undefined);
+      self.setState({render: true});
+
+    }
+
+  },
+
+  setPos: function(url, pos) {
+    this.setState({navPos: pos});
+    router.navigate(url);
+  },
+
+  setLoggedIn: function(state) {
+    this.setState({loggedIn: state});
+
+    // Kill cookie if user is logging out
+    if(state === undefined) {
+      $.removeCookie("application")
+    };
+
+  },
+
+  render: function() {
+
+// Defer rendering while checking if cookie is valid
+if(this.state.render) {
+
+    // Login view if not logged in
+    if(this.state.loggedIn === undefined) {
+
+      return (
+      <div>
+        <Login navPos={this.state.navPos} setPos={this.setPos} loggedIn={this.state.loggedIn} setLoggedIn={this.setLoggedIn} />
+      </div>
+      )
+    } else {
+
+    // User is logged in
+    return (
+      <div>
+      <TopBar navPos={this.state.navPos} setPos={this.setPos} loggedIn={this.state.loggedIn} setLoggedIn={this.setLoggedIn} />
+      <LeftMenu navLinks={this.props.navLinks} navPos={this.state.navPos} setPos={this.setPos} loggedIn={this.state.loggedIn} setLoggedIn={this.setLoggedIn} />
+      <Content navPos={this.state.navPos} setPos={this.setPos} loggedIn={this.state.loggedIn} setLoggedIn={this.setLoggedIn} />
+      </div>
+    );
+  }
+} else {
+
+  return(<div></div>)
+
+  }
+}
+
+});
+
+var Login = React.createClass({
+
+  getInitialState: function() {
+
+    return {email_blank: false, password_blank: false, server_error: undefined};
+
+  },
+
+  login: function(e) {
+
+    e.preventDefault();
+
+    var email = $("#email").val();
+    var password = $("#password").val();
+
+
+    // Validate - fields must not be empty
+    if(_.isEmpty(email)) {
+      this.setState({email_blank: true});
+    } else {
+      this.setState({email_blank: false});
+    };
+    
+    if(_.isEmpty(password)) {
+      this.setState({password_blank: true});
+    } else {
+      this.setState({password_blank: false});
+    };
+
+    // fields are populated
+    if(!_.isEmpty(email) && !_.isEmpty(password)) {
+      // TODO: check if credentials are valid
+
+      var self = this;
+
+      superagent
+      .post('http://localhost:9000/api/v1/auth/login')
+      //.send({ name: 'Manny', species: 'cat' })
+      .set('X-API-Email', email)
+      .set('X-API-Password', password)
+      .set('Accept', 'application/json')
+      .end(function(error, res){
+
+        var sessionId = JSON.parse(res.text).sessionId
+
+        console.log(res)
+        console.log(sessionId)
+        //console.log(document.cookie)
+
+        if(res.ok === true) {
+          self.props.setLoggedIn(email);
+
+          // set cookie
+          $.cookie("application", JSON.stringify({ "sessionId": sessionId,  "email": email }), {path: "/", expires: 120});
+
+        } else {
+
+          // Return error from server
+          self.setState({server_error: JSON.parse(res.text).error});
+                  
+
+        }
+
+      });
+
+    }
+
+  },
+
+  render: function() {
+    var self = this;
+
+    var email_blank = function() {
+      if(self.state.email_blank === true) {
+        return (
+          <small className="error">Email cannot be blank</small>
+        )
+      }
+    };
+
+    var password_blank = function() {
+      if(self.state.password_blank === true) {
+        return (
+          <small className="error">Password cannot be blank</small>
+        )
+      }
+    };
+
+    var server_error = function() {
+      if(self.state.server_error !== undefined) {
+        return (
+          <small className="error">{self.state.server_error}</small>
+        )
+      }
+    };
+
+    return (
+      <div id="login" className="row">
+        <div className="panel">
+          <h1>Login to Web App</h1>
+          Please login to access Web App.
+          <hr />
+          {server_error()}
+          <form>
+          <label>Email
+            <input id="email" type="text" placeholder="Email" />
+            {email_blank()}
+
+          </label>
+          <label>Password
+            <input id="password" type="password" placeholder="Password" />
+            {password_blank()}
+          </label>
+          <button onClick={this.login} className="button radius expand">Login</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
+
+
+var TopBar = React.createClass({
+
+  componentDidMount: function() {
+
+    $(document).foundation();
+
+  },
+
+route: function(event) {
+    event.preventDefault();
+    var url = $(event.target).attr('data-nav');
+    var pos = $(event.target).text();
+
+    this.props.setPos(url, pos);
+  },
+
+  logout: function(e) {
+
+    e.preventDefault();
+    this.props.setLoggedIn(undefined);
+
+  },
+
+  render: function() {
+
+    return (
+      <div id="topBar" className="sticky">
+      <nav className="top-bar" data-topbar>
+        <ul className="title-area">
+          <li className="name">
+            <h1><a href="#">Web App Name</a></h1>
+          </li>
+          <li className="toggle-topbar menu-icon"><a href="#">Menu</a></li>
+        </ul>
+
+        <section className="top-bar-section">
+
+          <ul className="right">
+            <li className="active has-dropdown">
+            <a href="#">Logged in as {this.props.loggedIn}</a>
+            <ul className="dropdown">
+                <li><a data-nav="account" onClick={this.route}>My Account</a></li>
+                <li><a onClick={this.logout}>Logout</a></li>
+              </ul>
+            </li>
+            <li className="has-dropdown">
+              <a href="#">Help</a>
+              <ul className="dropdown">
+                <li><a href="#">Getting Started Guide</a></li>
+                <li><a href="#">API Documentation</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </li>
+          </ul>
+
+        </section>
+      </nav>
+      </div>
+    );
+  }
+
+});
+
+
+var LeftMenu = React.createClass({
+
+route: function(event) {
+    event.preventDefault();
+    var url = $(event.target).attr('data-nav');
+    var pos = $(event.target).text();
+
+    this.props.setPos(url, pos);
+  },
+
+  render: function() {
+
+    var self = this;
+    
+    var links = _.map(this.props.navLinks, function(link, key) {
+
+      var classString = "";
+      if(self.props.navPos===link.name){classString = "active"};
+
+      return <li key={key}><a href={link.url} onClick={self.route} data-nav={link.url} className={classString}>{link.name}</a></li>
+    });
+
+    return (
+      <div id="leftMenu" className="small-4 large-2 columns">
+      <ul className="side-nav">{links}</ul>
+      </div>
+    );
+  }
+
+});
+
+var Content = React.createClass({
+
+  render: function() {
+    
+    var section = null;
+    if(this.props.navPos === "Dashboard") {
+      section = (
+        <Dashboard navPos={this.props.navPos} setPos={this.props.setPos} />
+      )
+    } else {
+      section = (
+        <p>{this.props.navPos}</p>
+      )
+    }
+
+    return (
+      <div id="content" className="small-8 large-10 columns">
+      {section}
+      </div>
+    )
+
+  }
+
+});
+
+var Router = Backbone.Router.extend({
+  routes : {
+    ""    : "index",
+    "campaigns" : "campaigns",
+    "account" : "account",
+    "*splat" : "index"
+  },
+    clearLayout : function() {
+        React.renderComponent(
+         <div></div>,
+      document.getElementById('application')
+    );
+    },
+  showLayout : function(pos) {
+
+    // Look up the current route against navigation_data so that we can populate Layout with the pretty navPos name.
+    var navPos = _.find(navigation_data, { 'url': pos });
+    if(_.has(navPos, "name")) {
+      navPos = navPos.name
+    } else {
+      navPos = "Dashboard"
+    }
+
+        React.renderComponent(
+      <Layout navLinks={navigation_data} navPos={navPos} />,
+      document.getElementById('application')
+    );
+    },
+  index : function() {
+    this.clearLayout();
+    this.showLayout("dashboard");
+  },
+  campaigns : function() {
+    this.clearLayout();
+    this.showLayout("campaigns");
+  },
+  account : function() {
+    this.clearLayout();
+    this.showLayout("account");
+  }
+});
+ 
+var router = new Router();
+ 
+Backbone.history.start({pushState: true});
