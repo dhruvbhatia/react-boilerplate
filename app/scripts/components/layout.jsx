6488024550c7dd5ccaa1f4e4576febde81131e6b
Dhruv Bhatia <dhruv@dhruvbhatia.com>
2014-04-30 23:46:27 +1000

    Refactored website selector code to make it less sucky

--- app/scripts/components/layout.jsx
+++ app/scripts/components/layout.jsx
@@ -1,11 +1,11 @@
 /** @jsx React.DOM */
 
 var navigation_data = [
-  {name: "Dashboard", url: "", showInMenu: true},
-  {name: "Websites", url: "websites", showInMenu: true},
-  {name: "Campaigns", url: "campaigns", showInMenu: true},
-  {name: "My Account", url: "account", subroutes: "Edit Account", showInMenu: true},
-  {name: "Edit Account", url: "account/edit", showInMenu: false}
+{name: "Dashboard", url: "", showInMenu: true},
+{name: "Websites", url: "websites", showInMenu: true},
+{name: "Campaigns", url: "campaigns", showInMenu: true},
+{name: "My Account", url: "account", subroutes: "Edit Account", showInMenu: true},
+{name: "Edit Account", url: "account/edit", showInMenu: false}
 ];
 
 
@@ -29,7 +29,6 @@ var Layout = React.createClass({
     if(!_.isUndefined(cookie)) {
 
       var token = JSON.parse(cookie).sessionId;
-      var active_website = JSON.parse(cookie).user.website;
 
       superagent
       .get('http://192.168.178.20:9000/api/v1/auth/validate/' + token)
@@ -40,14 +39,19 @@ var Layout = React.createClass({
         if(res.ok === true) {
 
           var sessionId = JSON.parse(res.text).sessionId
-        var user = JSON.parse(res.text).user
-        user.website = active_website;
-        user.websites = JSON.parse(res.text).websites
+          var user = JSON.parse(res.text).user
 
-          self.setState({loggedIn: user, render: true});
-          $.cookie("application", JSON.stringify({ "sessionId": sessionId, "user": user }), {path: "/", expires: 120});
+          user.websites = JSON.parse(res.text).websites
 
-        } else {
+        // set active website to the last selected website in the cookie, else first website in the user's list
+        var active_website = JSON.parse(cookie).user.website || _.first(user.websites).id;
+
+        self.setWebsite(active_website);
+
+        self.setState({loggedIn: user, render: true});
+        $.cookie("application", JSON.stringify({ "sessionId": sessionId, "user": user }), {path: "/", expires: 120});
+
+      } else {
 
           // Cookie no longer valid - render login form
           self.setLoggedIn(undefined);
@@ -77,11 +81,11 @@ var Layout = React.createClass({
   setWebsite: function(website) {
     this.setState({website: website});
 
-      var cookie = JSON.parse($.cookie("application"));
+    var cookie = JSON.parse($.cookie("application"));
 
-      cookie.user.website = website;
+    cookie.user.website = website;
 
-      $.cookie("application", JSON.stringify(cookie), {path: "/", expires: 120});
+    $.cookie("application", JSON.stringify(cookie), {path: "/", expires: 120});
 
 
   },
@@ -105,36 +109,36 @@ if(this.state.render) {
     if(this.state.loggedIn === undefined) {
 
       return (
-      <div>
-        <Login navPos={this.state.navPos} setPos={this.setPos} loggedIn={this.state.loggedIn} setLoggedIn={this.setLoggedIn} />
-      </div>
-      )
+              <div>
+              <Login navPos={this.state.navPos} setPos={this.setPos} loggedIn={this.state.loggedIn} setLoggedIn={this.setLoggedIn} />
+              </div>
+              )
     } else {
 
     // User is logged in
     return (
-      <div>
-      <TopBar navPos={this.state.navPos} setPos={this.setPos} loggedIn={this.state.loggedIn} setLoggedIn={this.setLoggedIn} website={this.state.website} setWebsite={this.setWebsite} />
-      <LeftMenu navLinks={this.props.navLinks} navPos={this.state.navPos} setPos={this.setPos} loggedIn={this.state.loggedIn} setLoggedIn={this.setLoggedIn} website={this.state.website} setWebsite={this.setWebsite} />
-      <Content navPos={this.state.navPos} setPos={this.setPos} loggedIn={this.state.loggedIn} setLoggedIn={this.setLoggedIn} website={this.state.website} setWebsite={this.setWebsite} />
-      </div>
-    );
+            <div>
+            <TopBar navPos={this.state.navPos} setPos={this.setPos} loggedIn={this.state.loggedIn} setLoggedIn={this.setLoggedIn} website={this.state.website} setWebsite={this.setWebsite} />
+            <LeftMenu navLinks={this.props.navLinks} navPos={this.state.navPos} setPos={this.setPos} loggedIn={this.state.loggedIn} setLoggedIn={this.setLoggedIn} website={this.state.website} setWebsite={this.setWebsite} />
+            <Content navPos={this.state.navPos} setPos={this.setPos} loggedIn={this.state.loggedIn} setLoggedIn={this.setLoggedIn} website={this.state.website} setWebsite={this.setWebsite} />
+            </div>
+            );
   }
 } else {
 
-var loadingMsgStyle = {
-         position: "relative",
-         top: "30%"
-     };
+  var loadingMsgStyle = {
+   position: "relative",
+   top: "30%"
+ };
 
-  return(
+ return(
 
-      <div className="row text-center" style={loadingMsgStyle}>
-      <h1>Loading...</h1>
-    </div>
-)
+        <div className="row text-center" style={loadingMsgStyle}>
+        <h1>Loading...</h1>
+        </div>
+        )
 
-  }
+}
 }
 
 });
@@ -202,7 +206,7 @@ var Login = React.createClass({
 
           // Return error from server
           self.setState({server_error: JSON.parse(res.text).error});
-                  
+
 
         }
 
@@ -218,49 +222,49 @@ var Login = React.createClass({
     var email_blank = function() {
       if(self.state.email_blank === true) {
         return (
-          <small className="error">Email cannot be blank</small>
-        )
+                <small className="error">Email cannot be blank</small>
+                )
       }
     };
 
     var password_blank = function() {
       if(self.state.password_blank === true) {
         return (
-          <small className="error">Password cannot be blank</small>
-        )
+                <small className="error">Password cannot be blank</small>
+                )
       }
     };
 
     var server_error = function() {
       if(self.state.server_error !== undefined) {
         return (
-          <small className="error">{self.state.server_error}</small>
-        )
+                <small className="error">{self.state.server_error}</small>
+                )
       }
     };
 
     return (
-      <div id="login" className="row">
-        <div className="panel">
-          <h1>Login to Web App</h1>
-          Please login to access Web App.
-          <hr />
-          {server_error()}
-          <form>
-          <label>Email
+            <div id="login" className="row">
+            <div className="panel">
+            <h1>Login to Web App</h1>
+            Please login to access Web App.
+            <hr />
+            {server_error()}
+            <form>
+            <label>Email
             <input id="email" type="text" placeholder="Email" />
             {email_blank()}
 
-          </label>
-          <label>Password
+            </label>
+            <label>Password
             <input id="password" type="password" placeholder="Password" />
             {password_blank()}
-          </label>
-          <button onClick={this.login} className="button radius expand">Login</button>
-          </form>
-        </div>
-      </div>
-    );
+            </label>
+            <button onClick={this.login} className="button radius expand">Login</button>
+            </form>
+            </div>
+            </div>
+            );
   }
 });
 
@@ -273,7 +277,7 @@ var TopBar = React.createClass({
 
   },
 
-route: function(event) {
+  route: function(event) {
     event.preventDefault();
     var url = $(event.target).attr('data-nav');
     var pos = $(event.target).text();
@@ -291,47 +295,47 @@ route: function(event) {
   render: function() {
 
     return (
-      <div id="topBar" className="sticky">
-      <nav className="top-bar" data-topbar data-options="is_hover: false">
-        <ul className="title-area">
-          <li className="name">
+            <div id="topBar" className="sticky">
+            <nav className="top-bar" data-topbar data-options="is_hover: false">
+            <ul className="title-area">
+            <li className="name">
             <h1><a href="#">Web App Name</a></h1>
-          </li>
-          <li className="toggle-topbar menu-icon"><a href="#">Menu</a></li>
-        </ul>
+            </li>
+            <li className="toggle-topbar menu-icon"><a href="#">Menu</a></li>
+            </ul>
 
-        <section className="top-bar-section">
+            <section className="top-bar-section">
 
-          <ul className="right">
+            <ul className="right">
             <li className="active has-dropdown">
             <a href="#">Logged in as {this.props.loggedIn.fullName}</a>
             <ul className="dropdown">
-                <li><a data-nav="account" onClick={this.route}>My Account</a></li>
-                <li><a onClick={this.logout}>Logout</a></li>
-              </ul>
+            <li><a data-nav="account" onClick={this.route}>My Account</a></li>
+            <li><a onClick={this.logout}>Logout</a></li>
+            </ul>
             </li>
             <li className="has-dropdown">
-              <a href="#">Help</a>
-              <ul className="dropdown">
-                <li><a href="#">Getting Started Guide</a></li>
-                <li><a href="#">API Documentation</a></li>
-                <li><a href="#">Contact Us</a></li>
-              </ul>
+            <a href="#">Help</a>
+            <ul className="dropdown">
+            <li><a href="#">Getting Started Guide</a></li>
+            <li><a href="#">API Documentation</a></li>
+            <li><a href="#">Contact Us</a></li>
+            </ul>
             </li>
-          </ul>
+            </ul>
 
-        </section>
-      </nav>
-      </div>
-    );
-  }
+            </section>
+            </nav>
+            </div>
+            );
+}
 
 });
 
 
 var LeftMenu = React.createClass({
 
-route: function(event) {
+  route: function(event) {
     event.preventDefault();
     var url = $(event.target).attr('data-nav');
     var pos = $(event.target).text();
@@ -352,11 +356,11 @@ route: function(event) {
     });
 
     return (
-      <div id="leftMenu" className="small-4 large-2 columns">
-      <Websites website={this.props.website} setWebsite={this.props.setWebsite} />
-      <ul className="side-nav">{links}</ul>
-      </div>
-    );
+            <div id="leftMenu" className="small-4 large-2 columns">
+            <Websites website={this.props.website} setWebsite={this.props.setWebsite} />
+            <ul className="side-nav">{links}</ul>
+            </div>
+            );
   }
 
 });
@@ -364,34 +368,9 @@ route: function(event) {
 
 var Websites = React.createClass({
 
-  componentDidMount: function() {
-
-    var websites = JSON.parse($.cookie("application")).user.websites;
-
-    if(JSON.parse($.cookie("application")).user.website === undefined) {
-
-       // set active website to the first website in the user's list if they haven't prev chosen an active website
-      var first_website = _.first(websites).id;
-
-      this.props.setWebsite(first_website);
-
-      console.log(first_website);
-
-      React.addons.TestUtils.Simulate.change($("#websiteSelector"),$("#websiteSelector").val(first_website));
-
-    } else {
-
-       React.addons.TestUtils.Simulate.change($("#websiteSelector"),$("#websiteSelector").val(JSON.parse($.cookie("application")).user.website));
-       this.props.setWebsite(JSON.parse($.cookie("application")).user.website);
-
-    
-    }
-
-  },
-
   websiteSelected: function(e) {
 
-    var websites = JSON.parse($.cookie("application")).user.websites
+    var websites = JSON.parse($.cookie("application")).user.websites;
 
     if(parseInt(e.target.value) === 0) {
 
@@ -410,31 +389,31 @@ var Websites = React.createClass({
   },
 
   render: function() {
+    var self = this;
+    if($.cookie("application")) {
+      var websites = JSON.parse($.cookie("application")).user.websites
 
-if($.cookie("application")) {
-    var websites = JSON.parse($.cookie("application")).user.websites
 
-
-     var links = _.map(websites, function(site, key) {
-      //console.log(site)
+      var links = _.map(websites, function(site, key) {
 
       return <option key={site.id} value={site.id}>{site.name}</option>
 
     });
 
-};
+    };
 
     return (
 
-      <div>
-      <select id="websiteSelector" onChange={this.websiteSelected}>
-      
-      <option key="0" value="0">Add New Website</option>
-      {links}
-      </select>
+            <div>
 
-      </div>
-    )
+            <select id="websiteSelector" onChange={this.websiteSelected} value={self.props.website}>
+
+            <option key="0" value="0">Add New Website</option>
+            {links}
+            </select>
+
+            </div>
+            )
 
   }
 
@@ -443,26 +422,26 @@ if($.cookie("application")) {
 var Content = React.createClass({
 
   render: function() {
-    
+
     var section = null;
     if(this.props.navPos === "Dashboard") {
       section = (
-        <Dashboard navPos={this.props.navPos} setPos={this.props.setPos} loggedIn={this.props.loggedIn} setLoggedIn={this.props.setLoggedIn} />
-      )
+                 <Dashboard navPos={this.props.navPos} setPos={this.props.setPos} loggedIn={this.props.loggedIn} setLoggedIn={this.props.setLoggedIn} />
+                 )
     } else if(this.props.navPos === "My Account") {
-        section = (
-          <MyAccount navPos={this.props.navPos} setPos={this.props.setPos} loggedIn={this.props.loggedIn} setLoggedIn={this.props.setLoggedIn} />
-        )
+      section = (
+                 <MyAccount navPos={this.props.navPos} setPos={this.props.setPos} loggedIn={this.props.loggedIn} setLoggedIn={this.props.setLoggedIn} />
+                 )
 
     }  else if(this.props.navPos === "Edit Account") {
-        section = (
-          <EditAccount navPos={this.props.navPos} setPos={this.props.setPos} loggedIn={this.props.loggedIn} setLoggedIn={this.props.setLoggedIn} />
-        )
+      section = (
+                 <EditAccount navPos={this.props.navPos} setPos={this.props.setPos} loggedIn={this.props.loggedIn} setLoggedIn={this.props.setLoggedIn} />
+                 )
 
     } else {
       section = (
-        <p>{this.props.navPos}</p>
-      )
+                 <p>{this.props.navPos}</p>
+                 )
     }
 
 
@@ -470,15 +449,15 @@ var Content = React.createClass({
     if(this.props.website === undefined) {
       console.log(this.props.website)
       section = (
-        <p>Create a website</p>
-      )
+                 <p>Create a website</p>
+                 )
     }
 
     return (
-      <div id="content" className="small-8 large-10 columns">
-      {section}
-      </div>
-    )
+            <div id="content" className="small-8 large-10 columns">
+            {section}
+            </div>
+            )
 
   }
 
@@ -493,12 +472,12 @@ var Router = Backbone.Router.extend({
     "account/edit" : "accountEdit",
     "*splat" : "index"
   },
-    clearLayout : function() {
-        React.renderComponent(
-         <div></div>,
-      document.getElementById('application')
-    );
-    },
+  clearLayout : function() {
+    React.renderComponent(
+                          <div></div>,
+                          document.getElementById('application')
+                          );
+  },
   showLayout : function(pos) {
 
     // Look up the current route against navigation_data so that we can populate Layout with the pretty navPos name.
@@ -509,11 +488,11 @@ var Router = Backbone.Router.extend({
       navPos = "Dashboard"
     }
 
-        React.renderComponent(
-      <Layout navLinks={navigation_data} navPos={navPos} />,
-      document.getElementById('application')
-    );
-    },
+    React.renderComponent(
+                          <Layout navLinks={navigation_data} navPos={navPos} />,
+                          document.getElementById('application')
+                          );
+  },
   index : function() {
     this.clearLayout();
     this.showLayout("dashboard");
@@ -535,7 +514,7 @@ var Router = Backbone.Router.extend({
     this.showLayout("account/edit");
   }
 });
- 
+
 var router = new Router();
- 
+
 Backbone.history.start({pushState: true});
