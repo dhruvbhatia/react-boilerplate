7e042954381f7697369f82fd29ea35d5e8ead53f
Dhruv Bhatia <dhruv@dhruvbhatia.com>
2014-06-16 23:27:08 +1000

    Working on campaign stream builer

--- app/scripts/components/campaigns.jsx
+++ app/scripts/components/campaigns.jsx
@@ -3,20 +3,51 @@
 
 var Campaigns = React.createClass({
 
-  getInitialState: function() {
+  componentDidMount: function() {
 
-    return {'elements' : []};
+    var self = this;
+
+    $(document).foundation('reveal', {animation: 'fade', animation_speed: 50});
 
   },
 
-  addElement: function(e) {
+  getInitialState: function() {
+
+    return {elements : [], counter : 0, modal: false};
+
+  },
+
+  addLateralElement: function(e) {
 
     e.preventDefault();
     var elementArray = this.state.elements;
-    console.log(elementArray);
-    elementArray.push(elementArray.length);
+    var counter = this.state.counter + 1;
+    if(elementArray.length === 0) {
+      counter = 1;
+    }
 
-    this.setState({'elements' : elementArray});
+    elementArray.push(counter);
+
+    this.setState({elements : elementArray, counter: counter});
+
+  },
+
+  blah: function() {
+
+    console.log("asd");
+
+  },
+
+  removeLateralElement: function(e) {
+    e.preventDefault();
+
+    var elementKey = parseInt($(e.target).closest('div').parent().attr('id'));
+
+    var filtered = _.filter(this.state.elements, function(element, key) {
+      return key !== elementKey;
+    });
+
+    this.setState({'elements' : filtered});
 
   },
 
@@ -24,13 +55,16 @@ var Campaigns = React.createClass({
 
     var self = this;
 
-    var elementChain = _.map(self.state.elements, function(element) {
+    var elementChain = _.map(self.state.elements, function(element, key) {
 
-            return(
-                   <div>
-                   <div className="panel radius noBottomMargin">{element}</div>
-                   <div className="connector"></div>
-                   </div>
+      return(
+             <div key={key} id={key}>
+             <div className="panel radius noBottomMargin">
+             {element}
+             <a onClick={self.removeLateralElement} className="right">&times;</a>
+             </div>
+             <div className="connector"></div>
+             </div>
 
              );
 
@@ -39,14 +73,24 @@ var Campaigns = React.createClass({
     return (
             <div>
             <p>{this.props.path}</p>
+            <a href="#" data-reveal-id="myModal">Click Me For A Modal</a>
             
 
             <div className="panel chart tall" id="ruleBuilder">
             <div className="panel radius noBottomMargin">Users</div>
             <div className="connector"></div>
             {elementChain}
-            <button className="button radius" onClick={this.addElement}>+</button>
+            <button className="button radius" onClick={self.addLateralElement}>+</button>
 
+            </div>
+
+
+            <div key="4" id="myModal" className="reveal-modal tiny" data-reveal>
+            <h2>Awesome. I have it.</h2>
+            <p className="lead">Your couch.  It is mine.</p>
+            <p>Im a cool paragraph that lives inside of an even cooler modal. Wins</p>
+            <button className="button radius" onClick={self.blah}>+</button>
+            <a className="close-reveal-modal">&#215;</a>
             </div>
 
             </div>
